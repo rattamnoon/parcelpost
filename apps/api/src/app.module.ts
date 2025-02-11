38d732bbd776c2781717160cf06867a73df2bcd4
UN <rattamnoon.kir@gmail.com>
2025-02-11 11:14:30 +0700

    create entity parcepost
    Signed-off-by: UN <rattamnoon.kir@gmail.com>

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -12,7 +13,8 @@ import { AppService } from './app.service';
       username: 'root',
       password: 'password',
       database: 'parcelpost',
-      entities: [],
+      entities: [join(__dirname, './database/entities/**/*{.ts,.js}')],
+      migrations: [join(__dirname, './database/migrations/**/*{.ts,.js}')],
       synchronize: false,
     }),
   ],
---
+++ apps/api/src/database/entities/parcelpost.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class ParcelPost {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true, comment: 'รหัสพัสดุที่สร้างเอง' })
+  code: string;
+
+  @Column({ comment: 'รหัสพัสดุ' })
+  parcelCode: string;
+
+  @Column({ comment: 'ชื่อผู้ส่ง', nullable: true })
+  senderName: string;
+
+  @Column({ comment: 'ชื่อผู้รับ', nullable: true })
+  receiverName: string;
+
+  @Column({ comment: 'ห้อง', nullable: true })
+  unitCode: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
